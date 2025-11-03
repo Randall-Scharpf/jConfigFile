3285e5198c02f7db7c962d47a0749ac87cc825e9
Randall Scharpf <randall.scharpf@gmail.com>
2025-11-02 22:31:42 -0800

    add ConfigInitializerPrompt

---
+++ src/com/randallscharpf/java/jconfigfile/ConfigInitializerPrompt.java
@@ -0,0 +1,205 @@
+package com.randallscharpf.java.jconfigfile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+
+/**
+ * Interactive CLI to allow a user to set up a configuration file in a standard location.
+ * 
+ * Allows the user to select an existing configuration file from an unsupported location
+ * and copy it to a supported location or to create a new, blank configuration file in a
+ * selected location. The API exposes methods to synchronously or asynchronously
+ * request the result of the interactive operation, which is a {@link ConfigFile}.
+ * The prompt can be constructed on any set of I/O streams.
+ */
+public class ConfigInitializerPrompt {
+
+    private static final Map<String, ConfigLocation> CONFIG_LOCATIONS = new HashMap<>();
+    private static final String SELECT_CONFIG_LOCATION_PROMPT;
+
+    static {
+        for (ConfigLocation loc : ConfigLocation.values()) {
+            CONFIG_LOCATIONS.put(loc.toString(), loc);
+        }
+        SELECT_CONFIG_LOCATION_PROMPT = generateConfigLocationPrompt();
+    }
+    
+    private static String generateConfigLocationPrompt() {
+        String result = "Select a location to place this application's config file. Valid options are ";
+        int i = 0;
+        for (String opt : CONFIG_LOCATIONS.keySet()) {
+            if (i == CONFIG_LOCATIONS.size() - 1) {
+                result += "and " + opt;
+            } else {
+                result += opt + ", ";
+            }
+            i++;
+        }
+        return result;
+    }
+
+    private final ConfigFinder finder;
+    private final Scanner userInputScanner;
+    private final PrintStream userOutput;
+    private final PrintStream userErrors;
+
+    private final Object stateKey;
+    private boolean active;
+
+    /**
+     * Creates a CLI on the given streams for the application described by the given
+     * {@link ConfigFinder}.
+     * 
+     * The CLI will not prompt the user or read input until triggered by a call to
+     * either {@code getInitializedFile} or {@code getInitializedFileAsync}. The
+     * output stream and error stream may be the same stream or may be different
+     * streams.
+     * 
+     * @param finder {@link ConfigFinder} for the application which needs its configuration initialized
+     * @param userInput character stream from which user inputs and selections come
+     * @param userOutput character stream to which prompts should be written
+     * @param userErrors character stream to which input parsing issues should be written
+     */
+    public ConfigInitializerPrompt(ConfigFinder finder, InputStream userInput, PrintStream userOutput, PrintStream userErrors) {
+        this.finder = finder;
+        this.userInputScanner = new Scanner(userInput);
+        this.userOutput = userOutput;
+        this.userErrors = userErrors;
+
+        this.stateKey = new Object();
+        this.active = false;
+    }
+
+    /**
+     * Begins CLI interactions and blocks until the user has handled all prompts,
+     * then returns the configuration produced by the user's selection.
+     * 
+     * User code must not call this method from multiple threads at once, or call this
+     * method while its asynchronous version is still awaiting user input.
+     * An {@code IllegalStateException} will be thrown if user code does not adhere
+     * to this requirement.
+     * 
+     * When the user selects and confirms the desired qualities of the configuration file
+     * to initialize, a {@link ConfigFile} will be created corresponding to the selected
+     * characteristics. If the attempt is successful, the configuration file will be
+     * returned, otherwise an exception is thrown. If the user cancels the operation,
+     * {@code null} will be returned.
+     * 
+     * @return an initialized configuration file located according to user input
+     * @throws IOException if initializing a configuration file as the user requests is impossible
+     * @throws IllegalStateException if the CLI is already being used to select a file
+     */
+    public ConfigFile getInitializedFile() throws IOException {
+        synchronized (stateKey) {
+            if (active) {
+                throw new IllegalStateException("This CLI is already being used to initialize a config file!");
+            }
+            active = true;
+        }
+        ConfigFile result = getInitializedFileUnguarded();
+        return result;
+    }
+
+    /**
+     * Begins CLI interactions and returns immediately, then later calls a provided callback
+     * with the configuration produced by the user's selection.
+     * 
+     * Once this method is called, user code must wait until the callback runs before
+     * calling it again or calling the synchronous version of this method.
+     * An {@code IllegalStateException} will be thrown if user code does not adhere
+     * to this requirement.
+     * 
+     * When the user selects and confirms the desired qualities of the configuration file
+     * to initialize, the program will attempt to create a {@link ConfigFile}
+     * corresponding to the selected characteristics.
+     * <ul>
+     * <li>
+     *   If the attempt is successful, {@code callback} will be executed with the
+     *   first parameter set to the created file and the second parameter set to {@code null}.
+     * </li>
+     * <li>
+     *   If the attempt is unsuccessful, {@code callback} will be executed with the
+     *   first parameter set to {@code null} and the second parameter set to an
+     *   appropriate exception describing the failure.
+     * </li>
+     * <li>
+     *   If the user cancels the operation, {@code callback} will be executed with
+     *   both parameters set to {@code null}.
+     * </li>
+     * </ul>
+     * 
+     * @param callback to be executed when the user interaction is complete
+     * @throws IllegalStateException if the CLI is already being used to select a file
+     */
+    public void getInitializedFileAsync(BiConsumer<ConfigFile, IOException> callback) {
+        synchronized (stateKey) {
+            if (active) {
+                throw new IllegalStateException("This CLI is already being used to initialize a config file!");
+            }
+            active = true;
+        }
+        new Thread(() -> {
+            try {
+                ConfigFile result = getInitializedFileUnguarded();
+                active = false;
+                callback.accept(result, null);
+            } catch (IOException ex) {
+                active = false;
+                callback.accept(null, ex);
+            }
+        }).start();
+    }
+    
+    private ConfigFile getInitializedFileUnguarded() throws IOException {
+        String location = prompt(SELECT_CONFIG_LOCATION_PROMPT, CONFIG_LOCATIONS::containsKey);
+        if (location == null) return null;
+        File newfile = finder.configAt(CONFIG_LOCATIONS.get(location));
+        boolean submenuCancelled;
+        do {
+            submenuCancelled = false;
+            String copy = prompt(
+                    "Is there an existing file to use to import configuration settings? Valid options are YES and NO",
+                    (str) -> { return "YES".equals(str) || "NO".equals(str); }
+            );
+            if (copy == null) return null;
+            if ("YES".equals(copy)) {
+                String oldfile = prompt("  ", "Enter the file path to copy", (path) -> { return new File(path).exists(); });
+                if (oldfile == null) {
+                    submenuCancelled = true;
+                } else {
+                    newfile.getParentFile().mkdirs();
+                    Files.copy(new File(oldfile).toPath(), newfile.toPath());
+                }
+            }
+        } while (submenuCancelled);
+        return new ConfigFile(newfile);
+    }
+    
+    private String prompt(String query, Predicate<String> validator) {
+        return prompt("", query, validator);
+    }
+
+    private String prompt(String prefix, String query, Predicate<String> validator) {
+        String resp = null;
+        do {
+            if (resp != null) {
+                userErrors.println(prefix + "Input \"" + resp + "\" was not accepted");
+            }
+            userOutput.println(prefix + query + " (or type 'cancel')");
+            System.out.print(prefix);
+            resp = userInputScanner.nextLine();
+            if ("cancel".equals(resp)) {
+                return null;
+            }
+        } while (!validator.test(resp));
+        return resp;
+    }
+}
--- src/com/randallscharpf/java/jconfigfile/InteractiveConfigInitializer.java
+++ src/com/randallscharpf/java/jconfigfile/InteractiveConfigInitializer.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.randallscharpf.java.jconfigfile;
 
 import java.io.File;
@@ -10,15 +6,41 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import javax.swing.JOptionPane;
 
+/**
+ * Class containing static utility methods to interactively initialize a configuration
+ * for a program. WARNING: This class should not be used in automated scripts where no
+ * user is present to select and confirm settings.
+ */
 public class InteractiveConfigInitializer {
-    
+
+    /**
+     * Synchronously find a config file or create one if none exists.
+     * 
+     * If a config file corresponding to the program-identifying parameters passed
+     * already exists, it is returned with no user interaction. Otherwise, the method
+     * will block until user input confirms the settings with which the config file
+     * is to be initialized. This method returns {@code null} if the user cancels
+     * the prompts with which they are provided.
+     * 
+     * In environments supporting graphical interfaces, a {@link ConfigInitializerDialog}
+     * will be displayed and used to interact with the user. In headless environments,
+     * a {@link ConfigInitializerPrompt} will interact with the user through {@code System.in}
+     * and {@code System.out}.
+     * 
+     * The arguments to this method are passed to {@link ConfigFinder#ConfigFinder}.
+     * 
+     * @param callee some class inside the application whose configuration will be located
+     * @param configName the name of the application whose configuration will be located
+     * @return a new or preexisting configuration file for the described application
+     * @throws IOException if the configuration file which was found or selected cannot be opened
+     */
     public static ConfigFile findOrCreateConfig(Class<?> callee, String configName) throws IOException {
         ConfigFinder finder = new ConfigFinder(callee, configName);
         File file = finder.searchForConfig();
         if (!file.exists()) {
             if (java.awt.GraphicsEnvironment.isHeadless()) {
-                // TODO: support headless environments by using terminal to set up config file
-                throw new java.awt.HeadlessException();
+                ConfigInitializerPrompt p = new ConfigInitializerPrompt(finder, System.in, System.out, System.err);
+                return p.getInitializedFile();
             } else {
                 ConfigInitializerDialog d = new ConfigInitializerDialog(finder);
                 return d.getInitializedFile();
@@ -27,14 +49,50 @@ public class InteractiveConfigInitializer {
             return new ConfigFile(file);
         }
     }
-    
+
+    /**
+     * Asynchronously find a config file or create one if none exists.
+     * 
+     * If a config file corresponding to the program-identifying parameters passed
+     * already exists, it is passed to the callback immediately with no user interaction.
+     * Otherwise, the method will return and later (once user input confirms the settings
+     * with which the config file is to be initialized) call the provided callback.
+     * 
+     * <ul>
+     * <li>
+     *   If the configuration file which was found or selected cannot be opened,
+     *   the first parameter to the callback will be {@code null} and the second
+     *   will provide the exception that occurred.
+     * </li>
+     * <li>
+     *   If the configuration file which was found or selected has been successfully
+     *   opened, it will be passed to the callback as the first parameter and the
+     *   second parameter will be {@code null}.
+     * </li>
+     * <li>
+     *   If the user cancels the prompts this method presents, the callback will
+     *   be called with both parameters set to {@code null}.
+     * </li>
+     * </ul>
+     * 
+     * In environments supporting graphical interfaces, a {@link ConfigInitializerDialog}
+     * will be displayed and used to interact with the user. In headless environments,
+     * a {@link ConfigInitializerPrompt} will interact with the user through {@code System.in}
+     * and {@code System.out}.
+     * 
+     * The first two arguments to this method are passed to {@link ConfigFinder#ConfigFinder}.
+     * 
+     * @param callee some class inside the application whose configuration will be located
+     * @param configName the name of the application whose configuration will be located
+     * @param callback to be executed when the user interaction is complete
+     */
     public static void findOrCreateConfigAsync(Class<?> callee, String configName, BiConsumer<ConfigFile, IOException> callback) {
         ConfigFinder finder = new ConfigFinder(callee, configName);
         File file = finder.searchForConfig();
         if (!file.exists()) {
             if (java.awt.GraphicsEnvironment.isHeadless()) {
-                // TODO: support headless environments by using terminal to set up config file
-                throw new java.awt.HeadlessException();
+                ConfigInitializerPrompt p = new ConfigInitializerPrompt(finder, System.in, System.out, System.err);
+                p.getInitializedFileAsync(callback);
             } else {
                 ConfigInitializerDialog d = new ConfigInitializerDialog(finder);
                 d.getInitializedFileAsync(callback);
@@ -48,40 +106,94 @@ public class InteractiveConfigInitializer {
             }
         }
     }
-    
+
+/**
+     * Synchronously find a config file or create one if none exists.
+     * 
+     * If a config file corresponding to the program-identifying parameters passed
+     * already exists, it is returned with no user interaction. Otherwise, the method
+     * will block until user input confirms the settings with which the config file
+     * is to be initialized. This method presents a warning to the user and returns
+     * a {@link ConfigMap} if a persistent {@link ConfigFile} cannot be created.
+     * 
+     * In environments supporting graphical interfaces, a {@link ConfigInitializerDialog}
+     * will be displayed and used to interact with the user. In headless environments,
+     * a {@link ConfigInitializerPrompt} will interact with the user through {@code System.in}
+     * and {@code System.out}.
+     * 
+     * The arguments to this method are passed to {@link ConfigFinder#ConfigFinder}.
+     * 
+     * @param callee some class inside the application whose configuration will be located
+     * @param configName the name of the application whose configuration will be located
+     * @return a new or preexisting configuration file for the described application
+     */
     public static Config findOrCreateConfigWithFallback(Class<?> callee, String configName) {
         try {
             Config cfg = findOrCreateConfig(callee, configName);
             if (cfg == null) {
-                JOptionPane.showMessageDialog(null,
-                        "User closed config initializer dialog without creating config file"
-                        + "\nConfiguration changes will not be persistent.",
-                        "Error", JOptionPane.WARNING_MESSAGE);
+                String message = "User canceled config initializer without creating config file"
+                        + "\nConfiguration changes will not be persistent.";
+                if (java.awt.GraphicsEnvironment.isHeadless()) {
+                    System.err.println(message);
+                } else {
+                    JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.WARNING_MESSAGE);
+                }
                 return new ConfigMap();
             }
             return cfg;
         } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null,
-                    "Failed to open config file with message: " + ex.getMessage()
-                    + "\nConfiguration changes will not be persistent.",
-                    "Error", JOptionPane.ERROR_MESSAGE);
+            String message = "Failed to open config file with message: " + ex.getMessage()
+                    + "\nConfiguration changes will not be persistent.";
+            if (java.awt.GraphicsEnvironment.isHeadless()) {
+                System.err.println(message);
+            } else {
+                JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+            }
             return new ConfigMap();
         }
     }
-    
+
+    /**
+     * Asynchronously find a config file or create one if none exists.
+     * 
+     * If a config file corresponding to the program-identifying parameters passed
+     * already exists, it is passed to the callback immediately with no user interaction.
+     * Otherwise, the method will return and later (once user input confirms the settings
+     * with which the config file is to be initialized) call the provided callback.
+     * 
+     * This method presents a warning to the user and passes a {@link ConfigMap} to
+     * {@code callback} if a persistent {@link ConfigFile} cannot be created.
+     * 
+     * In environments supporting graphical interfaces, a {@link ConfigInitializerDialog}
+     * will be displayed and used to interact with the user. In headless environments,
+     * a {@link ConfigInitializerPrompt} will interact with the user through {@code System.in}
+     * and {@code System.out}.
+     * 
+     * The first two arguments to this method are passed to {@link ConfigFinder#ConfigFinder}.
+     * 
+     * @param callee some class inside the application whose configuration will be located
+     * @param configName the name of the application whose configuration will be located
+     * @param callback to be executed when the user interaction is complete
+     */
     public static void findOrCreateConfigAsyncWithFallback(Class<?> callee, String configName, Consumer<Config> callback) {
         findOrCreateConfigAsync(callee, configName, (res, err) -> {
             if (res == null) {
                 if (err != null) {
-                    JOptionPane.showMessageDialog(null,
-                            "Failed to open config file with message: " + err.getMessage() +
-                            "\nConfiguration changes will not be persistent.",
-                            "Error", JOptionPane.ERROR_MESSAGE);
+                    String message = "Failed to open config file with message: " + err.getMessage()
+                            + "\nConfiguration changes will not be persistent.";
+                    if (java.awt.GraphicsEnvironment.isHeadless()) {
+                        System.err.println(message);
+                    } else {
+                        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+                    }
                 } else {
-                    JOptionPane.showMessageDialog(null,
-                            "User closed config initializer dialog without creating config file" +
-                            "\nConfiguration changes will not be persistent.",
-                            "Error", JOptionPane.WARNING_MESSAGE);
+                    String message = "User canceled config initializer without creating config file"
+                            + "\nConfiguration changes will not be persistent.";
+                    if (java.awt.GraphicsEnvironment.isHeadless()) {
+                        System.err.println(message);
+                    } else {
+                        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.WARNING_MESSAGE);
+                    }
                 }
                 callback.accept(new ConfigMap());
             } else {
